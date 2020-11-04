92f70f68c0b1f62c6af6a84601b8ab1132031490
Cristian Tabacitu <cristitabacitu@gmail.com>
2020-11-04 13:52:03 -0400

    added TODOs for making the AddonServiceProvider automatic

--- src/AddonServiceProvider.php
+++ src/AddonServiceProvider.php
@@ -13,9 +13,16 @@ class AddonServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // TODO: if lang folder exists and is not empty
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', ':lc:vendor');
+        
+        // TODO: if views folder exists and is not empty
         $this->loadViewsFrom(__DIR__.'/../resources/views', ':lc:vendor');
+
+        // TODO: if migrations folder exists and is not empty
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        
+        // TODO: if routes folder exists and is not empty
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         // Publishing is only necessary when using the CLI.
@@ -31,6 +38,7 @@ class AddonServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // TODO: if config folder exists and is not empty
         // $this->mergeConfigFrom(__DIR__.'/../config/:lc:package.php', ':lc:package');
     }
 
@@ -41,26 +49,31 @@ class AddonServiceProvider extends ServiceProvider
      */
     protected function bootForConsole(): void
     {
+        // TODO: if config folder exists and is not empty
         // Publishing the configuration file.
         // $this->publishes([
         //     __DIR__.'/../config/:lc:package.php' => config_path(':lc:package.php'),
         // ], 'config');
 
+        // TODO: if views folder exists and is not empty
         // Publishing the views.
         $this->publishes([
             __DIR__.'/../resources/views' => base_path('resources/views/vendor/:lc:vendor'),
         ], 'views');
 
+        // TODO: if assets folder exists and is not empty
         // Publishing assets.
         /*$this->publishes([
             __DIR__.'/../resources/assets' => public_path('vendor/:lc:vendor'),
         ], 'assets');*/
 
+        // TODO: if lang folder exists and is not empty
         // Publishing the translation files.
         /*$this->publishes([
             __DIR__.'/../resources/lang' => resource_path('lang/vendor/:lc:vendor'),
         ], 'lang');*/
 
+        // TODO: if commands array exists and is not empty
         // Registering package commands.
         // $this->commands([]);
     }
